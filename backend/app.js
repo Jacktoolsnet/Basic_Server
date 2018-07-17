4739f80ff66f9d096e5ea62e9b8b5481fab7e221
Chrisitan Wagner <jacktoolsnet@gmail.com>
2018-07-17 20:07:33 +0200

    CORS
    Add CORS headers

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next();
+});
+
+app.use((req, res, next) => {
   res.send('Hello from express');
 });
 
